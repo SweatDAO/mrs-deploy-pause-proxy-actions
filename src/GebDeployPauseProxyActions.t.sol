b9640e0cef66efe38f996d2fc1dadc3606a19ba1
fabiohild <fabiohildebrand@gmail.com>
2020-10-09 18:28:13 -0300

    adding missing tests for new functions

--- src/GebDeployPauseProxyActions.t.sol
+++ src/GebDeployPauseProxyActions.t.sol
@@ -46,12 +46,37 @@ contract ProxyCalls {
     function updateRedemptionRate(address, address, address, bytes32, uint256) public {
         proxy.execute(proxyActions, msg.data);
     }
+
+    function setProtester(address pause, address actions, address protester) public {
+        proxy.execute(proxyActions, msg.data);
+    }
+
+    function setDelay(address pause, address actions, uint newDelay) public {
+        proxy.execute(proxyActions, msg.data);
+    }
+
+    function setDelayMultiplier(address pause, address actions, uint delayMultiplier) public {
+        proxy.execute(proxyActions, msg.data);
+    }
+
+    function setTotalAllowance(address pause, address actions, address who, address account, uint rad) public {
+        proxy.execute(proxyActions, msg.data);
+    }
+
+    function setPerBlockAllowance(address pause, address actions, address who, address account, uint rad) public {
+        proxy.execute(proxyActions, msg.data);
+    }
+
+    function setAllowance(address pause, address actions, address join, address account, uint allowance) public {
+        proxy.execute(proxyActions, msg.data);
+    }
 }
 
 contract GebDeployPauseProxyActionsTest is GebDeployTestBase, ProxyCalls {
     bytes32 collateralAuctionType = bytes32("ENGLISH");
 
     function setUp() override public {
+        PAUSE_TYPE = keccak256(abi.encodePacked("PROTEST"));
         super.setUp();
         deployStable(collateralAuctionType);
         DSProxyFactory factory = new DSProxyFactory();
@@ -104,7 +129,7 @@ contract GebDeployPauseProxyActionsTest is GebDeployTestBase, ProxyCalls {
         assertEq(taxPercentage, 10 ** 27);
     }
 
-    function testRely() public {
+    function testAddAuthorization() public {
         assertEq(oracleRelayer.authorizedAccounts(address(123)), 0);
         this.addAuthorization(address(pause), address(govActions), address(oracleRelayer), address(123));
         assertEq(oracleRelayer.authorizedAccounts(address(123)), 1);
@@ -137,5 +162,48 @@ contract GebDeployPauseProxyActionsTest is GebDeployTestBase, ProxyCalls {
         this.setAuthorityAndDelay(address(pause), address(govActions), address(123), 5);
         assertEq(address(pause.authority()), address(123));
         assertEq(pause.delay(), 5);
+    }
+
+    function testSetProtester() public {
+        assertEq(address(pause.protester()), address(0));
+        this.setProtester(address(pause), address(govActions), address(124));
+        assertEq(address(pause.protester()), address(124));
+    }
+
+    function testSetDelay() public {
+        assertEq(pause.delay(), 0);
+        this.setDelay(address(pause), address(govActions), 1024);
+        assertEq(pause.delay(), 1024);
+    }
+
+    function testSetDelayMultiplier() public {
+        assertEq(pause.delayMultiplier(), 1);
+        this.setDelayMultiplier(address(pause), address(govActions), 3);
+        assertEq(pause.delayMultiplier(), 3);
+    }
+
+    function testSetTotalAllowance() public {
+        uint allowance = 25 * 10 ** 45;
+        (uint total,) = stabilityFeeTreasury.getAllowance(address(1));
+        assertEq(total, 0);
+        this.setTotalAllowance(address(pause), address(govActions), address(stabilityFeeTreasury), address(1), allowance);
+        (total,) = stabilityFeeTreasury.getAllowance(address(1));
+        assertEq(total, allowance);
+    }
+
+    function testSetPerBlockAllowance() public {
+        uint allowance = 25 * 10 ** 45;
+        (, uint perBlock) = stabilityFeeTreasury.getAllowance(address(1));
+        assertEq(perBlock, 0);
+        this.setPerBlockAllowance(address(pause), address(govActions), address(stabilityFeeTreasury), address(1), allowance);
+        (, perBlock) = stabilityFeeTreasury.getAllowance(address(1));
+        assertEq(perBlock, allowance);
+    }
+
+    function testSetAllowance() public {
+        uint allowance = 25 * 10 ** 45;
+        assertEq(colJoin.allowance(address(1)), 0);
+        this.setAllowance(address(pause), address(govActions), address(colJoin), address(1), allowance);
+        assertEq(colJoin.allowance(address(1)), allowance);
     }
 }
